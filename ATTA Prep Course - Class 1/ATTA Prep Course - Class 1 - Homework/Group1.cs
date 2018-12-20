eba518dff9e9102cd2720a0a5ebc84436d86140d
elendae42 <46009770+elendae42@users.noreply.github.com>
2018-12-20 15:59:32 -0800

    Frame work for Week/Class 1 clean up and selection menu for that week - Exercises not yet implemented (#2)

---
+++ ATTA Prep Course - Class 1/ATTA Prep Course - Class 1 - Homework/Group1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATTA_Prep_Course___Class_1___Homework
+{
+    class Group1
+    {
+        // Basics, Types and Variables
+        // Ex 1. Create a new Visual C# Console application and name it whatever you wish. Within the Main() function, 
+        // write a message to the console by calling the Console.WriteLine() function.
+        static void exWriteLine(string userMessage)
+        {
+            Console.WriteLine(userMessage);
+        }
+    }
+}
